146975de1edf173401eb2196416c26568ba1361a
Andy <andy@coggle.it>
2018-10-19 22:49:28 +0700

    implement gesture recogniser state machine

--- ios/RCTTouchHandler+Touch.m
+++ ios/RCTTouchHandler+Touch.m
@@ -1,4 +1,5 @@
 #import <UIKit/UIKit.h>
+#import <UIKit/UIGestureRecognizerSubclass.h>
 #import <React/RCTTouchHandler.h>
 #import <React/RCTEventDispatcher.h>
 #import <React/RCTBridge+Private.h>
@@ -9,18 +10,32 @@
 
 @import CobrowseIO;
 
+static uint16_t _coalescingKey = 0;
+
 @implementation RCTTouchHandler (Touch)
 
 - (void) cobrowseTouchesBegan:(NSSet<CBIOTouch*> *)touches withEvent:(CBIOTouchEvent *)event {
+    _coalescingKey++;
     [self cobrowseDispatchEvent:@"touchStart" withTouches: touches withEvent:event];
+    _coalescingKey++;
+    if (self.state == UIGestureRecognizerStatePossible)
+        self.state = UIGestureRecognizerStateBegan;
 }
 
 - (void) cobrowseTouchesMoved:(NSSet<CBIOTouch*> *)touches withEvent:(CBIOTouchEvent *)event {
     [self cobrowseDispatchEvent:@"touchMove" withTouches: touches withEvent:event];
+    self.state = UIGestureRecognizerStateChanged;
 }
 
 - (void) cobrowseTouchesEnded:(NSSet<CBIOTouch*> *)touches withEvent:(CBIOTouchEvent *)event {
+    _coalescingKey++;
     [self cobrowseDispatchEvent:@"touchEnd" withTouches: touches withEvent:event];
+    _coalescingKey++;
+    self.state = UIGestureRecognizerStateEnded;
+}
+
+- (void) cobrowseTouchesCancelled:(NSSet<CBIOTouch*> *)touches withEvent:(CBIOTouchEvent *)event {
+    self.state = UIGestureRecognizerStateCancelled;
 }
 
 -(void) cobrowseDispatchEvent: (NSString*) eventName withTouches:(NSSet<CBIOTouch*> *)touches withEvent: (CBIOTouchEvent*) event {
@@ -30,23 +45,28 @@
     CBIOTouch* touch = touches.allObjects.firstObject;
     NSNumber* reactTag = self.view.reactTag;
     
-    CGPoint viewCoords = [touch.target convertPoint:event.position fromView: nil];
+    UIView *targetView = touch.target;
+    while (targetView) {
+        if (targetView.reactTag && targetView.userInteractionEnabled) break;
+        targetView = targetView.superview;
+    }
+    
+    CGPoint viewCoords = [targetView convertPoint:event.position fromView: nil];
     
     NSMutableDictionary *reactTouch = [NSMutableDictionary dictionary];
-    reactTouch[@"target"] = touch.target.reactTag;
+    reactTouch[@"target"] = targetView.reactTag;
     reactTouch[@"identifier"] = @(1);
-    reactTouch[@"pageX"] = @(RCTSanitizeNaNValue(roundf(event.position.x), @"touchEvent.pageX"));
-    reactTouch[@"pageY"] = @(RCTSanitizeNaNValue(roundf(event.position.y), @"touchEvent.pageY"));
-    reactTouch[@"locationX"] = @(RCTSanitizeNaNValue(roundf(viewCoords.x), @"touchEvent.locationX"));
-    reactTouch[@"locationY"] = @(RCTSanitizeNaNValue(roundf(viewCoords.y), @"touchEvent.locationY"));
+    reactTouch[@"pageX"] = @(RCTSanitizeNaNValue(event.position.x, @"touchEvent.pageX"));
+    reactTouch[@"pageY"] = @(RCTSanitizeNaNValue(event.position.y, @"touchEvent.pageY"));
+    reactTouch[@"locationX"] = @(RCTSanitizeNaNValue(viewCoords.x, @"touchEvent.locationX"));
+    reactTouch[@"locationY"] = @(RCTSanitizeNaNValue(viewCoords.y, @"touchEvent.locationY"));
     reactTouch[@"timestamp"] =  @([NSProcessInfo.processInfo systemUptime] * 1000); // in ms, for JS
     
-    static uint16_t _coalescingKey = 0;
     RCTTouchEvent* reactEvent = [[RCTTouchEvent alloc] initWithEventName:eventName
                                                          reactTag:reactTag
                                                      reactTouches:@[reactTouch]
                                                    changedIndexes:@[@(0)]
-                                                    coalescingKey:_coalescingKey++];
+                                                    coalescingKey:_coalescingKey];
     
     RCTEventDispatcher* dispatcher = [[RCTBridge currentBridge] moduleForClass:[RCTEventDispatcher class]];
     [dispatcher sendEvent: reactEvent];
