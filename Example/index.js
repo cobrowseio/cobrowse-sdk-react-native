110994320e8b31b145e572a17d9714f0eabff8a6
Andy <andy@coggle.it>
2019-11-12 11:17:35 +0000

    dependency updates

--- Example/index.js
+++ Example/index.js
@@ -11,21 +11,3 @@ AppRegistry.registerComponent(appName, () => App);
 import CobrowseIO from 'cobrowse-sdk-react-native';
 CobrowseIO.license = "trial";
 CobrowseIO.start();
-
-
-
-CobrowseIO.handleSessionRequest = function(session) {
-    Alert.alert(
-        'Session Requested',
-        'A cobrowse session has been requested',
-        [
-            {
-                text: 'Cancel',
-                onPress: () => {},
-                style: 'cancel',
-            },
-            {text: 'OK', onPress: () => CobrowseIO.activateSession()},
-        ],
-        {cancelable: true},
-    );
-}
