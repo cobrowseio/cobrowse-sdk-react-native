e3f312354b9e4672814413e57fe1aaa38d547df5
Andy <andy@coggle.it>
2020-03-06 14:36:33 +0000

    update example to latest RN

--- Example/index.js
+++ Example/index.js
@@ -2,12 +2,12 @@
  * @format
  */
 
-import {AppRegistry, Alert} from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
 
 import CobrowseIO from 'cobrowse-sdk-react-native';
-CobrowseIO.license = "trial";
+CobrowseIO.license = 'trial';
 CobrowseIO.start();
