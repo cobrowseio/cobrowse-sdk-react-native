736d366a69c6f65e4de7a8d42ab98f795b53bca5
Andy <andy@cobrowse.io>
2021-02-16 17:24:30 +0000

    add useContext to ensure redaction is only applied at the top level

--- js/Redacted.js
+++ js/Redacted.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, requireNativeComponent } from 'react-native';
 const CBIOCobrowseRedacted = requireNativeComponent('CBIOCobrowseRedacted');
 
+const RedactionContext = React.createContext(false);
+
 module.exports = function(props) {
-    return (
-        <CBIOCobrowseRedacted><View>{props.children}</View></CBIOCobrowseRedacted>
+    const alreadyRedacted = useContext(RedactionContext);
+    if (!alreadyRedacted) return (
+        <RedactionContext.Provider value={true}>
+            <CBIOCobrowseRedacted>
+                <View>{props.children}</View>
+            </CBIOCobrowseRedacted>
+        </RedactionContext.Provider>
     );
+    return props.children;
 }
