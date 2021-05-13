9c7c7b32b25fd9de5d0a8a6844ccb031616977dd
Aaron Huggins <aaron.huggins@runbox.com>
2021-05-13 18:28:27 -0500

    Improve scaffolding - Add dev dependencies for typescript - ts linting config - Fix linting issues  -Add CobrowseView declaration

--- ts/CobrowseIO.d.ts
+++ ts/CobrowseIO.d.ts
@@ -1,38 +1,39 @@
+/* eslint-disable @typescript-eslint/no-extraneous-class,accessor-pairs */
 import type { EmitterSubscription } from 'react-native'
 
 // TODO: find out what this interface is in 'io.cobrowse' jar.
 export type Session = any
 
 export default class CobrowseIO {
-    static get SESSION_UPDATED(): 'session_updated'
+  static get SESSION_UPDATED (): 'session_updated'
 
-    static get SESSION_ENDED(): 'session_ended'
+  static get SESSION_ENDED (): 'session_ended'
 
-    static get SESSION_REQUESTED(): 'session_requested'
+  static get SESSION_REQUESTED (): 'session_requested'
 
-    static handleSessionRequest(session?: any): void
+  static handleSessionRequest (session?: any): void
 
-    static addListener(eventType: string, listener: (event: any) => void, context?: Object): EmitterSubscription
+  static addListener (eventType: string, listener: (event: any) => void, context?: Object): EmitterSubscription
 
-    static start(): void
+  static start (): void
 
-    static stop(): void
+  static stop (): void
 
-    static set api(api: string): void
+  static set api (api: string)
 
-    static set license(license: string): void
+  static set license (license: string)
 
-    static set customData(customData: Record<string, any>): void
+  static set customData (customData: Record<string, any>)
 
-    static set deviceToken(token: string): void
+  static set deviceToken (token: string)
 
-    static currentSession(): Promise<Session>
+  static currentSession (): Promise<Session>
 
-    static createSession(): Promise<Session>
+  static createSession (): Promise<Session>
 
-    static activateSession(): Promise<Session>
+  static activateSession (): Promise<Session>
 
-    static getSession(codeOrId: string): Promise<Session>
+  static getSession (codeOrId: string): Promise<Session>
 
-    static endSession(): Promise<void>
+  static endSession (): Promise<void>
 }
---
+++ ts/CobrowseView.d.ts
@@ -0,0 +1,22 @@
+import type { Component } from 'react'
+import type { View, Text } from 'react-native'
+
+export default class CobrowseView extends Component {
+  constructor (): CobrowseView
+
+  async componentDidMount (): Promise<void>
+
+  componentWillUnmount (): void
+
+  async endSession (): Promise<void>
+
+  renderError (): Text
+
+  renderCode (): View
+
+  renderManageSession (): View
+
+  renderContent (): Text | View
+
+  render (): View
+}
