599d7fadba100986876ffc624291a69e2e0f3fb0
Aaron Huggins <aaron.huggins@runbox.com>
2021-05-12 22:04:12 -0500

    Add declaration for CobrowseIO

---
+++ ts/CobrowseIO.d.ts
@@ -0,0 +1,38 @@
+import type { EmitterSubscription } from 'react-native'
+
+// TODO: find out what this interface is in 'io.cobrowse' jar.
+export type Session = any
+
+export default class CobrowseIO {
+    static get SESSION_UPDATED(): 'session_updated'
+
+    static get SESSION_ENDED(): 'session_ended'
+
+    static get SESSION_REQUESTED(): 'session_requested'
+
+    static handleSessionRequest(session?: any): void
+
+    static addListener(eventType: string, listener: (event: any) => void, context?: Object): EmitterSubscription
+
+    static start(): void
+
+    static stop(): void
+
+    static set api(api: string): void
+
+    static set license(license: string): void
+
+    static set customData(customData: Record<string, any>): void
+
+    static set deviceToken(token: string): void
+
+    static currentSession(): Promise<Session>
+
+    static createSession(): Promise<Session>
+
+    static activateSession(): Promise<Session>
+
+    static getSession(codeOrId: string): Promise<Session>
+
+    static endSession(): Promise<void>
+}
