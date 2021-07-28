4f767a5ac9fb63c9f3afa82d94b6ece8ecfb5f08
Andy <andy@cobrowse.io>
2021-07-28 15:03:08 +0100

    chore: example app update

--- Example/App.js
+++ Example/App.js
@@ -1,52 +1,57 @@
-import React, { Component } from 'react';
-import { TouchableOpacity, Platform, StyleSheet, Text, View, TextInput } from 'react-native';
-import { CobrowseView, Redacted, SessionControl, redact } from 'cobrowse-sdk-react-native';
+import React, { Component } from 'react'
+import { TouchableOpacity, Platform, StyleSheet, Text, View, TextInput } from 'react-native'
+import { CobrowseView, Redacted, SessionControl, redact } from 'cobrowse-sdk-react-native'
+import MyComponent from './MyComponent'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
-});
+})
 
-type Props = {};
+type Props = {}
 export default class App extends Component<Props> {
 
-    constructor() {
-        super()
-        this.state = { show: 1 };
-    }
+  constructor() {
+    super()
+    this.state = { show: 1 }
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <TouchableOpacity onPress={() => {
-            this.setState({ show: (this.state.show + 1) })
+          this.setState({ show: (this.state.show + 1) })
         }} style={styles.welcome}><Text>Welcome to React Native!</Text></TouchableOpacity>
         { this.state.show % 3 ? <View style={styles.container}>
-            <Redacted>
-                <View><Text style={styles.instructions}>To get started, edit App.js</Text></View>
-                { this.state.show % 2 ? <View><Text>This is a sibling!</Text></View> : null }
-            </Redacted>
-            <Text style={styles.welcome}>
-                <Text>This is the parent text</Text>
-                <Redacted style={{fontWeight: 'bold'}}><Text>This is the child text</Text></Redacted>
-            </Text>
-            <Redacted style={{
-                position: 'absolute',
-                top: 0,
-                left: 20
-            }}>
-                <Text>Floating</Text>
-            </Redacted>
-            <TextInput defaultValue={'Hello!'} onChange={() => {}} />
-            <Text style={styles.instructions}>{instructions}</Text>
-            <SessionControl><Text>Session is active</Text></SessionControl>
-            <SessionControl><View style={styles.floating}><Text>Overlay</Text></View></SessionControl>
-            <CobrowseView onEnded={() => {}} />
+          <Redacted>
+            <View><Text style={styles.instructions}>To get started, edit App.js</Text></View>
+            { this.state.show % 2 ? <View><Text>This is a sibling!</Text></View> : null }
+          </Redacted>
+          <Text style={styles.welcome}>
+            <Text>This is the parent text</Text>
+            <Redacted style={{fontWeight: 'bold'}}><Text>This is the child text</Text></Redacted>
+          </Text>
+          <MyComponent>
+            <Redacted><Text>Redacted inner text</Text></Redacted>
+            <Text>Some children</Text>
+          </MyComponent>
+          <Redacted style={{
+            position: 'absolute',
+            top: 0,
+            left: 20
+          }}>
+            <Text>Floating</Text>
+          </Redacted>
+          <TextInput defaultValue={'Hello!'} onChange={() => {}} />
+          <Text style={styles.instructions}>{instructions}</Text>
+          <SessionControl><Text>Session is active</Text></SessionControl>
+          <SessionControl><View style={styles.floating}><Text>Overlay</Text></View></SessionControl>
+          <CobrowseView onEnded={() => {}} />
         </View> : null }
       </View>
-    );
+    )
   }
 }
 
@@ -77,4 +82,4 @@ const styles = StyleSheet.create({
     height: 60,
     backgroundColor: '#00ff00',
   },
-});
+})
