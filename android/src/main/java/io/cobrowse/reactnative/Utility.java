71b5e92965d11b94b6de758e08dcdc289bf47c3f
Aaron Huggins <aaron@cobrowse.io>
2021-11-12 15:26:43 -0600

    Serialize remote_control

--- android/src/main/java/io/cobrowse/reactnative/Utility.java
+++ android/src/main/java/io/cobrowse/reactnative/Utility.java
@@ -5,8 +5,24 @@ import com.facebook.react.bridge.WritableMap;
 
 import io.cobrowse.Agent;
 import io.cobrowse.Session;
+import io.cobrowse.Session.RemoteControlState;
 
 final class Utility {
+
+    static String remoteControl (RemoteControlState state) {
+        switch (state) {
+            case RemoteControlState.On:
+                return "on";
+            case RemoteControlState.Requested:
+                return "requested";
+            case RemoteControlState.Rejected:
+                return "rejected";
+            case RemoteControlState.Off:
+                return "off";
+        }
+
+        return "off";
+    }
 
     static WritableMap convert(Session session) {
         WritableMap map = Arguments.createMap();
@@ -15,6 +31,7 @@ final class Utility {
         map.putString("state", session.state());
         map.putString("id", session.id());
         map.putBoolean("full_device", session.fullDevice());
+        map.putString("remote_control", Utility.remoteControl(session.remoteControl()));
 
         Agent agent = session.agent();
         if (agent != null) {
