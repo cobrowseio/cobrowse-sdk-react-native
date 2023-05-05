81d0abf7a6d627670f319501a8d62a6b899eb682
Joao Ribeiro <922529+jribeiro@users.noreply.github.com>
2023-05-05 21:54:30 +0200

    chore: fix NativeEventEmitter warning

--- android/src/main/java/io/cobrowse/reactnative/CobrowseIOModule.java
+++ android/src/main/java/io/cobrowse/reactnative/CobrowseIOModule.java
@@ -253,4 +253,14 @@ public class CobrowseIOModule extends ReactContextBaseJavaModule {
 
     CobrowseIO.instance().setDelegate(delegatesWithSessionControls);
   }
+
+  @ReactMethod
+  public void addListener(String eventName) {
+    // Keep: Required for RN built in Event Emitter Calls.
+  }
+
+  @ReactMethod
+  public void removeListeners(Integer count) {
+    // Keep: Required for RN built in Event Emitter Calls.
+  }
 }
