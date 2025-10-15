606ba9c5e0af8c6ccc997d5345001eef22d74c6f
Joao Ribeiro <922529+jribeiro@users.noreply.github.com>
2025-10-15 12:06:15 +0100

    fix: ensure redacted views are not collapsed by RN

--- js/Redacted.tsx
+++ js/Redacted.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { requireNativeComponent, type ViewProps } from 'react-native'
 
-const CBIOCobrowseRedacted = requireNativeComponent('CBIOCobrowseRedacted')
+const CBIOCobrowseRedacted = requireNativeComponent<ViewProps>('CBIOCobrowseRedacted')
 
-export default function Redacted (props: ViewProps): JSX.Element {
-  return <CBIOCobrowseRedacted {...props}>{props.children}</CBIOCobrowseRedacted>
+export default function Redacted(props: ViewProps): JSX.Element {
+  return <CBIOCobrowseRedacted {...props} collapsable={false}>{props.children}</CBIOCobrowseRedacted>
 }
